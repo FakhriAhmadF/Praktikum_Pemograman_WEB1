78177129564754c5fee6d40d373bb2fca52b339c
FakhriAhmadF <20230910046@uniku.ac.id>
2025-01-18 16:38:52 +0700

    Add files via upload

---
+++ Modul_09/Latihan_09_Home.php
@@ -0,0 +1,19 @@
+<h3>Selamat Datang di Halaman Utama</h3>
+<hr>
+<div class="container">
+    <p>Ini adalah halaman utama dari website Anda. Anda dapat menambahkan informasi atau fitur lainnya di sini sesuai kebutuhan.</p>
+    <div class="row mt-4">
+        <div class="col-md-6">
+            <h4>Informasi Terbaru</h4>
+            <ul>
+                <li>Pengumuman acara reuni alumni pada bulan depan.</li>
+                <li>Fitur baru: Pendaftaran alumni secara online.</li>
+                <li>Database alumni diperbarui.</li>
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <h4>Tentang Website</h4>
+            <p>Website ini dibuat untuk mengelola informasi tentang alumni. Anda dapat melihat daftar alumni, menambahkan data alumni baru, dan melakukan pengeditan atau penghapusan data.</p>
+        </div>
+    </div>
+</div>
