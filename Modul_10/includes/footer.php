b6f00e58fc0a74e1f3ec675eb88be7c8c2a0cce4
FakhriAhmadF <20230910046@uniku.ac.id>
2025-01-19 19:23:39 +0700

    Create footer.php

---
+++ Modul_10/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="bg-light text-center py-3">
+    <p>&copy; <?php echo date('Y'); ?> FakhriAhmadF. All rights reserved.</p>
+</footer>
+<!-- Bootstrap JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
