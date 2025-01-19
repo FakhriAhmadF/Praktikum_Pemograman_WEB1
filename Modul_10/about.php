d105bdaaaa145302eb9bf74be29bf33420027bff
FakhriAhmadF <20230910046@uniku.ac.id>
2025-01-19 18:58:40 +0700

    Menambahkan file baru

---
+++ Modul_10/about.php
@@ -0,0 +1,26 @@
+<?php include 'includes/header.php'; ?>
+<div class="container my-5">
+    <h2>About Me</h2>
+    <div class="row align-items-center">
+        <div class="col-md-4">
+            <img src="assets/img/Profile.jpeg" alt="Fakhri Ahmad Fakhrudin" class="img-fluid rounded-circle shadow">
+        </div>
+        <div class="col-md-8">
+            <p>Nama saya Fakhri Ahmad Fakhrudin, seorang mahasiswa Program Studi Sistem Informasi di Universitas Kuningan. Saya memiliki ketertarikan dalam bidang teknologi informasi dan tertarik untuk mengembangkan solusi yang dapat mempermudah kehidupan sehari-hari. Selain itu, saya juga memiliki hobi menggambar, yang membantu saya untuk mengekspresikan diri dan mengasah kreativitas.</p>
+            <p>Tujuan utama saya adalah untuk terus mengembangkan diri, baik di bidang akademis maupun dalam kehidupan pribadi. Saya berusaha untuk menerapkan keterampilan teknis yang saya pelajari di bangku kuliah untuk berkontribusi dalam dunia profesional, serta menggabungkannya dengan kreativitas saya dalam desain dan pengembangan produk digital. Saya selalu berusaha untuk menjadi pribadi yang terbuka, mudah beradaptasi, dan terus belajar.</p>
+            <p>Melalui berbagai proyek dan pengalaman, saya berharap dapat mencapai tujuan untuk menjadi profesional yang dapat memberikan dampak positif bagi masyarakat.<hr></p>
+        </div>
+    </div>
+    <p style="margin-top: 50px;">Berikut sedikit tentang saya:</p>
+    <ul>
+        <li><strong>Education:</strong> Memperdalam pengetahuan dalam sistem informasi, teknologi perangkat lunak.</li>
+        <li><strong>Skills:</strong>
+            <ol>
+                <li>Design: Kemampuan menggambar dan desain grafis untuk membuat visualisasi yang menarik dan informatif (Sedikit)</li>
+                <li>Web Development: Pengembangan website dengan menggunakan framework seperti React dan Laravel (Dicoding Mengakui)</li>
+            </ol>
+        </li>
+        <li><strong>Career Goals:</strong> Punya pekerjaan yang layak 👍</li>
+    </ul>
+</div>
+<?php include 'includes/footer.php'; ?>
